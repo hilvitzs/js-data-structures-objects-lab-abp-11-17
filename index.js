4f115d83f96bf73cfcd79c27529febe4b5a280be
Spencer Hilvitz <spencer.hilvitz@gmail.com>
2017-11-09 21:05:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,4 +13,6 @@ function deleteFromDriverByKey(driver, key) {
   return Object.assign({}, { name: undefined })
 }
 
-function destructivelyDeleteFromDriverByKey
+function destructivelyDeleteFromDriverByKey() {
+  delete driver.name;
+}
